b9bd93232927c407db7d59c8bd0bcf3a2f2f044a
VetrovAnton228 <55150813+Anton554@users.noreply.github.com>
2022-07-07 22:07:38 +0300

    Реальзованна ассинхронная работа парсера

--- pars_ist.py
+++ pars_ist.py
@@ -1,52 +1,41 @@
-import asyncio
+import multiprocessing
 from random import randint
-import aiohttp
-from bs4 import BeautifulSoup
+import requests
 from lxml import html
 
 
-async def ist(n):
-    # n = randint(1, 617)
-    async with aiohttp.ClientSession() as session:
-        rec = await session.get(f'https://eku.ru/category/story/?page={str(n)}')
-        soup = BeautifulSoup(await rec.text(), "html.parser")
-    HtmlTree = html.fromstring(str(soup))
-    async with aiohttp.ClientSession() as session:
-        rec = await session.get('https://eku.ru' + HtmlTree.xpath(f"//div[@class='item-list humor-item']/h3/a/@href")[0])
-        soup = BeautifulSoup(await rec.text(), "html.parser")
-    HtmlTree = html.fromstring(str(soup))
+def ist(n):
+    rec = requests.get(f'https://eku.ru/category/story/?page={str(n)}')
+    HtmlTree = html.fromstring(rec.content)
+    rec = requests.get('https://eku.ru' + HtmlTree.xpath(f"//div[@class='item-list humor-item']/h3/a/@href")[0])
+    HtmlTree = html.fromstring(rec.content)
 
     zag = HtmlTree.xpath(f"//div[@id='content']/h1/text()")
-    # zag = [el.encode('ISO-8859-1').decode('UTF-8') for el in zag]
+    zag = [el.encode('ISO-8859-1').decode('UTF-8') for el in zag]
     txt = HtmlTree.xpath(f"//div[@class='text-content']/p/text()")
-    # txt = [el.encode('ISO-8859-1').decode('UTF-8') + '\n' for el in txt]
+    txt = [el.encode('ISO-8859-1').decode('UTF-8') + '\n' for el in txt]
     return ''.join(zag) + '\n' + '\n' + ''.join(txt)
 
 
-# def anegdot():
-#     vr = randint(0, 1)
-#     if vr == 0:
-#         num = randint(2, 194)
-#         response = requests.get(f'https://anekdotov.net/anekdot/one/random{num}.html')
-#         HtmlTree = html.fromstring(response.content)
-#         el = HtmlTree.xpath(f"//a[@href='/anekdot/one/random{num - 1}.html']/text()")
-#         return '\n'.join(el[:-2])
-#     else:
-#         num = randint(1, 100)
-#         response = requests.get(f'https://anekdotbar.ru/top-100.html')
-#         HtmlTree = html.fromstring(response.content)
-#         el = HtmlTree.xpath(f"//div[@class ='tecst'][{num}]/text()")
-#         return '\n'.join(el[:-1])
-
-
-async def main():
-    ls = [randint(1, 617) for _ in range(30)]
-    ls_coroutines = [ist(n) for n in ls]
-    for cor in asyncio.as_completed(ls_coroutines):
-        cor_result = await cor
-        print(cor_result)
+def anegdot():
+    vr = randint(0, 1)
+    if vr == 0:
+        num = randint(2, 194)
+        response = requests.get(f'https://anekdotov.net/anekdot/one/random{num}.html')
+        HtmlTree = html.fromstring(response.content)
+        el = HtmlTree.xpath(f"//a[@href='/anekdot/one/random{num - 1}.html']/text()")
+        return '\n'.join(el[:-2])
+    else:
+        num = randint(1, 100)
+        response = requests.get(f'https://anekdotbar.ru/top-100.html')
+        HtmlTree = html.fromstring(response.content)
+        el = HtmlTree.xpath(f"//div[@class ='tecst'][{num}]/text()")
+        return '\n'.join(el[:-1])
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
+    if __name__ == '__main__':
+        with multiprocessing.Pool(10) as pool:
+            print('Результаты:')
+            print(pool.map(ist, range(200, 220)))
+
